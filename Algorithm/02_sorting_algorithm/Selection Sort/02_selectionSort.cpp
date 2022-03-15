cbeae3fc9da24295de4a9ebdfe2ed2e438b019fd
surajgirioffl <surajkumargiri2002@gmail.com>
2022-03-15 07:37:11 +0530

    added program of selection sort using cpp

---
+++ Algorithm/02_sorting_algorithm/Selection Sort/02_selectionSort.cpp
@@ -0,0 +1,124 @@
+﻿/*selection sort using two ways.
+ *1. swaping perform in inner for loop each time if condition is true.
+ *2. swaping perform just after termination of inner loop.
+ */
+
+/*
+ *author: Suraj Kumar Giri.
+ *Date: 15-03-2022
+ *Time: 04:45:30
+ */
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+/*for sorting the array using selection sort*/
+class selectionSort
+{
+    int *array;
+    int size;
+    void creatArray();
+    void algorithm1();
+    void algorithm2();
+    void displaySortedArray();
+
+public:
+    void sortTheArray(int choice)
+    {
+        creatArray();
+        if (choice == 1)
+        {
+            algorithm1();
+        }
+        else
+            algorithm2();
+        displaySortedArray();
+        free(array); // free memory
+    }
+};
+
+/*to create the array dynamically*/
+void selectionSort::creatArray()
+{
+    cout << "write the size of array:" << endl;
+    fflush(stdin);
+    cin >> size;
+    array = (int *)calloc(size, sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
+        cout << "write number of index " << i << ":" << endl;
+        cin >> array[i];
+    }
+}
+
+/*sorting using selection sort. By swaping outside the inner for loop.*/
+void selectionSort::algorithm1()
+{
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int minimum = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[minimum] > array[j])
+                minimum = j;
+        }
+
+        if (i != minimum) // it's (only if statement) not compulsory. But O(1) (in case of array[i] is smallest) is better than O(4).
+        {
+            /*It's working*/
+            int temp = array[i];
+            array[i] = array[minimum];
+            array[minimum] = temp;
+
+            /*It's not working. ?*/
+            // int temp = array[minimum];
+            // array[minimum] = array[i];
+            // array[i] == temp;
+        }
+    }
+}
+
+/*sorting using selection sort. By swaping inside the inner for loop.*/
+void selectionSort::algorithm2()
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[i] > array[j]) // performing swaping
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+/*to display the sorted array*/
+void selectionSort::displaySortedArray()
+{
+    cout << "Sorted Array is following:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << "\t";
+    }
+}
+
+int main()
+{
+    system("cls");
+    selectionSort obj;
+    cout << "Press 1 for sort the array via algorithm 1 of selection i.e swaping in inner loop." << endl;
+    cout << "Press 2 for sort the array via algorithm 2 of selection i.e swaping outside of inner loop." << endl;
+    int choice = 1;
+    cout << "$ ";
+    cin >> choice;
+    if (choice < 1 || choice > 2)
+        obj.sortTheArray(1);
+    else
+        obj.sortTheArray(choice);
+    return 0;
+}
