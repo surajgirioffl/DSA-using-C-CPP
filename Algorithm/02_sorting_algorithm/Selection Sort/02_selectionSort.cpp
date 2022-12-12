58bad0e16121192374179e30b405d7a936462bab
surajgirioffl <surajkumargiri2002@gmail.com>
2022-12-12 14:45:06 +0530

    Fix: Fix some spelling mistakes
    Fix some spelling mistakes.
    
    Signed-off-by: @surajgirioffl

--- Algorithm/02_sorting_algorithm/Selection Sort/02_selectionSort.cpp
+++ Algorithm/02_sorting_algorithm/Selection Sort/02_selectionSort.cpp
@@ -1,6 +1,6 @@
 ﻿/*selection sort using two ways.
- *1. swaping perform in inner for loop each time if condition is true.
- *2. swaping perform just after termination of inner loop.
+ *1. swapping perform in inner for loop each time if condition is true.
+ *2. swapping perform just after termination of inner loop.
  */
 
 /*
@@ -52,7 +52,7 @@ void selectionSort::creatArray()
     }
 }
 
-/*sorting using selection sort. By swaping outside the inner for loop.*/
+/*sorting using selection sort. By swapping outside the inner for loop.*/
 void selectionSort::algorithm1()
 {
 
@@ -80,14 +80,14 @@ void selectionSort::algorithm1()
     }
 }
 
-/*sorting using selection sort. By swaping inside the inner for loop.*/
+/*sorting using selection sort. By swapping inside the inner for loop.*/
 void selectionSort::algorithm2()
 {
     for (int i = 0; i < size - 1; i++)
     {
         for (int j = i + 1; j < size; j++)
         {
-            if (array[i] > array[j]) // performing swaping
+            if (array[i] > array[j]) // performing swapping
             {
                 int temp = array[i];
                 array[i] = array[j];
@@ -111,8 +111,8 @@ int main()
 {
     system("cls");
     selectionSort obj;
-    cout << "Press 1 for sort the array via algorithm 1 of selection i.e swaping in inner loop." << endl;
-    cout << "Press 2 for sort the array via algorithm 2 of selection i.e swaping outside of inner loop." << endl;
+    cout << "Press 1 for sort the array via algorithm 1 of selection i.e swapping in inner loop." << endl;
+    cout << "Press 2 for sort the array via algorithm 2 of selection i.e swapping outside of inner loop." << endl;
     int choice = 1;
     cout << "$ ";
     cin >> choice;
